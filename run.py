3d7e4fef7199d6f5caaf3aa03e2f6eb5946b00ca
Clayton Condie <clayton.condie@gmail.com>
2016-09-14 21:12:52 -0600

    pre image attack complete

--- run.py
+++ run.py
@@ -3,8 +3,11 @@ import random
 import string
 import time
 
+
 def sha1_wrapper(input, bit_len):
     """
+    takes in a string of any length and returns a truncated version of the digest
+
     :param input: string to hash with sha1
     :param bit_len: 1-160
     :return: sha1 hash of input truncated down to a sub_string of [0:bit_len]
@@ -13,14 +16,17 @@ def sha1_wrapper(input, bit_len):
     h.update(input.encode())
     digest = h.hexdigest()
 
-    bin_digest_array = []
-    for el in digest:
-        bin_digest_array.append(format(int(el, 16), '04b'))
-
-    bin_digest = ''.join(bin_digest_array)
+    bin_digest = hexdigest_to_string(digest)
     short_digest = bin_digest[0:bit_len]
 
     return short_digest
+
+
+def hexdigest_to_string(digest):
+    bin_digest_array = []
+    for el in digest:
+        bin_digest_array.append(format(int(el, 16), '04b'))
+    return ''.join(bin_digest_array)
 
 
 def random_word(length):
@@ -32,22 +38,53 @@ def collision_attack(bit_len, word_len):
     collision_found = False
 
     start = time.time()
+    word_count = 0
     while not collision_found:
         word = random_word(word_len)
         word_hash = sha1_wrapper(word, bit_len)
+        word_count += 1
+
         if word_hash in words_by_hash:
-            end = time.time()
+            delta = time.time() - start
+            collision_found = True
             print()
-            delta = end - start
-            print('COLLISION FOUND in ' + str(delta))
+            print('COLLISION FOUND in ' + str(delta) + ' after ' + str(word_count) + ' tries')
             print(word + ': ' + sha1_wrapper(word, bit_len))
             print(words_by_hash[word_hash] + ': ' + sha1_wrapper(words_by_hash[word_hash], bit_len))
-            collision_found = True
         else:
             words_by_hash[word_hash] = word
 
 
+def pre_image_attack(match_hash_str, hash_len, word_len):
+    """
+
+    :param match_hash_str: string value pre-hashing
+    :param hash_len: truncation length of the sha1 output from match_hash_str
+    :param word_len: word size to gen words to check for matching hashes
+    :return:
+    """
+    match_hash_bin = sha1_wrapper(match_hash_str, hash_len)
+
+    start = time.time()
+    match_found = False
+    word_count = 0
+    while not match_found:
+        word = random_word(word_len)
+        word_hash = sha1_wrapper(word, hash_len)
+        word_count += 1
+
+        if word_hash == match_hash_bin:
+            delta = time.time() - start
+            match_found = True
+            print()
+            print('MATCH FOUND in ' + str(delta) + ' after ' + str(word_count) + ' tries')
+            print('matched word: ' + word)
+            print('matched hash: ' + sha1_wrapper(word, hash_len))
+            print('inputed hash: ' + match_hash_bin)
+
 def main():
-    collision_attack(35, 30)
+
+    pre_image_attack('this is a massage', 20, 30)
+
 
 main()
